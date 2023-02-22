9ad2822e9b7b124f56acbb938549a91e6c4320a9
Rina <huvian11@mail.ru>
2023-02-22 14:48:10 +0300

    Done binding

--- app/src/main/java/com/example/prac2/MainActivity.java
+++ app/src/main/java/com/example/prac2/MainActivity.java
@@ -4,19 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.prac2.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
-    final private String TAG = this.getLocalClassName();
+    final private String TAG = "Main Activity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(
+                getLayoutInflater()
+        );
+        setContentView(binding.getRoot());
+        // setContentView(R.layout.activity_main);
+
+        binding.companyName.setText("Binding!");
+        binding.logo.setImageResource(R.drawable.ssssssssssssssssssss);
+
+        binding.enterName.setOnKeyListener(new View.OnKeyListener()
+                                           {
+                                               public boolean onKey(View v, int keyCode, KeyEvent event)
+                                               {
+                                                   if(event.getAction() == KeyEvent.ACTION_DOWN &&
+                                                           (keyCode == KeyEvent.KEYCODE_ENTER))
+                                                   {
+                                                       String strCatName = binding.enterName.getText().toString();
+                                                       binding.next.setVisibility(View.VISIBLE);
+                                                       return true;
+                                                   }
+                                                   return false;
+                                               }
+                                           }
+        );
 
         TextView nameOfCompany = (TextView)findViewById(R.id.company_name);
         TextView enterName = (TextView)findViewById(R.id.entering_mes);
@@ -25,15 +51,26 @@ public class MainActivity extends AppCompatActivity {
         ImageView logo = (ImageView)findViewById(R.id.logo);
         Button googleRegistration = (Button)findViewById(R.id.google);
 
-        nameOfCompany.setText(R.string.someName);
-        logo.setImageResource(R.drawable.ssssssssssssssssssss);
-        notRegister.setOnClickListener(new View.OnClickListener() { //программный способ установки слушателя
-            @Override
-            public void onClick(View v) {
-                enterName.setVisibility(View.VISIBLE);
-                enteringName.setVisibility(View.VISIBLE);
-            }
-        });
+//        nameOfCompany.setText(R.string.someName);
+//        logo.setImageResource(R.drawable.ssssssssssssssssssss);
+
+        binding.anon.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View v) {
+                                                enterName.setVisibility(View.VISIBLE);
+                                                enteringName.setVisibility(View.VISIBLE);
+                                            }
+                                        }
+
+        );
+
+//        notRegister.setOnClickListener(new View.OnClickListener() { //программный способ установки слушателя
+//            @Override
+//            public void onClick(View v) {
+//                enterName.setVisibility(View.VISIBLE);
+//                enteringName.setVisibility(View.VISIBLE);
+//            }
+//        });
     }
 
     public void continueWithGoogle(View view) { //декларативный способ установки слушателя
