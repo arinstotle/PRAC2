1e6ac7bd2adeca7ce96504d6ff45310672541195
Rina <huvian11@mail.ru>
2023-02-22 11:40:17 +0300

    The components were initialized in the program code, the text and image for the TextView and ImageView, respectively, were defined

--- app/src/main/java/com/example/prac2/MainActivity.java
+++ app/src/main/java/com/example/prac2/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.prac2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView nameOfCompany = (TextView)findViewById(R.id.company_name);
+        Button notRegister = (Button)findViewById(R.id.anon);
+        ImageView logo = (ImageView)findViewById(R.id.logo);
+        Button googleRegistration = (Button)findViewById(R.id.google);
+
+        nameOfCompany.setText(R.string.someName);
+        logo.setImageResource(R.drawable.ssssssssssssssssssss);
+
     }
 }
